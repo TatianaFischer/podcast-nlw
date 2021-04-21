135776e93177ad84a75cb01f8fc380a5598385c5
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-04-21 18:42:01 -0300

    [feat] SSG

--- podcast/src/pages/index.tsx
+++ podcast/src/pages/index.tsx
@@ -24,6 +24,7 @@ export default function Home(props) {
 
 //SSG: mas se a home nao sofre alterações com frequencia, não tem pq toda vez que carregar a página chamar a API, por isso tem como gerar uma versão estática de html, qualquer pessoa que acessar vai acessar o mesmo conteudo, deixando a pagina muito mais perfomatica:
 //o revalidate é o tempo para atuzaliar a pagina, a cada tanto tempo uma nova versão acessando a api vai ser gerada , no exemplo a cada 8h vai acessar a API.
+//essa forma só ocorre em produção, por isso foi criada um build.
 
     export async function getStaticProps() {
       const response = await fetch('http://localhost:3333/episodes')
