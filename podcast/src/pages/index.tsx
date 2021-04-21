e0285249bf387d8973dcd3f5791e651573325014
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-04-21 18:41:31 -0300

    [feat] SSG

--- podcast/src/pages/index.tsx
+++ podcast/src/pages/index.tsx
@@ -1,7 +1,39 @@
-import { Header } from "../components/Header";
+import { useEffect } from "react"
 
-export default function Home() {
+
+export default function Home(props) {
+  
   return (
    <h1>Index</h1>
+  
   )
 }
+//SPA: useEffect com .then ...
+//SSR - vai carregar sempre que alguem acessar, mesmo com o js desabilitado, vai ser carregado pelo nextjs.
+    // export async function getServerSideProps() {
+    //   const response = await fetch('http://localhost:3333/episodes')
+    //   const data = await response.json()
+      
+    //   return {
+    //     props: {
+    //       episodes: data,
+    //     }
+    //   }
+      
+    // }
+
+//SSG: mas se a home nao sofre alterações com frequencia, não tem pq toda vez que carregar a página chamar a API, por isso tem como gerar uma versão estática de html, qualquer pessoa que acessar vai acessar o mesmo conteudo, deixando a pagina muito mais perfomatica:
+//o revalidate é o tempo para atuzaliar a pagina, a cada tanto tempo uma nova versão acessando a api vai ser gerada , no exemplo a cada 8h vai acessar a API.
+
+    export async function getStaticProps() {
+      const response = await fetch('http://localhost:3333/episodes')
+      const data = await response.json()
+      
+      return {
+        props: {
+          episodes: data,
+        },
+        revalidate: 60 * 60 * 8,
+      }
+      
+    }
