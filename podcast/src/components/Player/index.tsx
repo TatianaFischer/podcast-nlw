1257ccc3318100d4e0bad7b49eb3c6dadba1ecc0
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-04-25 17:05:58 -0300

    [style] slider + music playing

--- podcast/src/components/Player/index.tsx
+++ podcast/src/components/Player/index.tsx
@@ -1,5 +1,9 @@
 
+import Image from 'next/image';
 import { useContext } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
@@ -12,18 +16,42 @@ export function Player() {
     <div className={styles.playerContainer}>
       <header>
         <img src='/playing.svg' alt='Tocando agora'/>
-        <strong>Tocando agora {episode?.title}</strong>
+        <strong>Tocando agora </strong>
       </header>
-
+    { episode ? 
+    (
+      <div className={styles.currentEpisode}>
+        <Image 
+          width={592} 
+          height={592} 
+          src={episode.thumbnail} 
+          objectFit="cover"
+        />
+        <strong>{episode.title}</strong>
+        <strong>{episode.members}</strong>
+      </div>
+      
+    ): (
       <div className={styles.emptyPlayer}>
         <strong> Selecione um podcast para ouvir</strong>
       </div>
 
-      <footer className={styles.empty}>
+    ) }
+      
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
+          {episode ? (
+            <Slider
+              trackStyle={{backgroundColor: '#04d361'}}
+              railStyle={{backgroundColor: '#9f75ff'}}
+              handleStyle={{backgroundColor: '#04d361', borderWidth: 4}}
+            />
+            ) : (
             <div className={styles.emptySlider}/>
+          )}
+            
           </div>          
           <span>00:00</span>
         </div>
